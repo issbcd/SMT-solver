164f7b5807b13856a094e00b40be5490026e269b
CarolinaCavalcantee <accj@ic.ufal.br>
2026-06-04 22:08:07 -0300

    atualizacao inicial TeoriaLIA

--- SMT_solver.h
+++ SMT_solver.h
@@ -7,56 +7,60 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define UNDEFINED 2
+#define UNDEFINED -1 
+#define MAX_VARS 64 /*vms declarar o max de variaveis q uma unica equacao pode ter*/
+
+typedef enum{/*usamos essa struct p nomear os numeros inteiros, ent o pc vai ler soma como o numero 0, e assim por diante*/
+    OP_MENORQ,/*<*/
+    OP_MAIORQ,/*>*/
+    OP_MENOR_IGUAL,/*<=*/
+    OP_MAIOR_IGUAL,/*>=*/
+    OP_IGUAL,/*==*/
+    OP_DIFERENTE,/*!=*/
+}Operador;
+
+typedef struct ExpressaoLIA{
+    int coeficientes[MAX_VARS];/*vai guardar os multiplicadores, tipo  2*X1, guarda o 2 */
+    int n_vars;/*quantas variaveis estao na expressao*/
+    Operador op; /*qual o simbolo da operacao*/
+    int constante; /*o numero do lado direito, tipo o 10 em x+y=10*/
+}ExpressaoLIA;
+
+typedef struct TeoriaLIA{/*vai guardar tds as equacoes do problema e os valores atuais*/
+ExpressaoLIA *restricoes;/*array dinamico q guarda tds as equacoes lidas*/
+int total;/*numero total de equacoes do array*/
+int n_vars;/*numero de variaveis inteiras distintas no problema inteiro*/
+int *valores_int; /*guarda o valor numerico de cada teste p cada variavel*/
+}TeoriaLIA;
 
 typedef struct partial_interpretation{
     int *atributions; 
-    int tam_variavel;/*medimos o tamanho limite do array aqui*/
 }partial_interpretation;
 
-typedef struct tree 
-{ 
+typedef struct tree{ 
     int variable; 
     int value; 
-    bool eh_folha; /*retorna 1 ou 0 p saber se chegamos a uma decisao final*/
-
     struct tree* left; 
     struct tree* right; 
 } tree;
-
-typedef enum{/*usamos essa struct p nomear os numeros inteiros, ent o pc vai ler soma como o numero 0, e assim por diante*/
-    op_soma,
-    op_subtracao,/*usamos a virgula pq estamos separando elementos de uma lista*/
-    op_multi,
-    op_divisao,
-    op_igual,
-    op_variavel,
-    op_const,
-    op_maiorque,
-    op_menorque,
-}op_mat;
-
-typedef struct expmat{
-op_mat tipo;
-int valor;
-
-    struct expmat* left; 
-    struct expmat* right; 
-}expmat;
-
-typedef struct teoria_smt{
-    char **expressoes;
-    int total_expressoes;
-}teoria_smt;
-
-void readcnffile (const char* filename, CNF* problem, teoria_smt *teoria);
-bool imprimir(tree *node, int total_literals);
-void free_tree(tree *node);
-void free_cnf(CNF *formula);
+/*declaracao das funcoes de fluxo booleano */
 partial_interpretation inicio_partial_interpretation(CNF *formula);
 bool eh_sat(CNF *formula, partial_interpretation *options);
 bool eh_unsat(CNF *formula, partial_interpretation *options);
 partial_interpretation unir(partial_interpretation *inicio, int total_literals, int value, int xi);
-tree *resposta_smt(CNF *formula, partial_interpretation now, teoria_smt *teoria);
-bool checagem(partial_interpretation *options, teoria_smt *teoria);
+
+/*declaracao das funcoes da TeoriaLIA*/
+bool parse_expressao_lia(const char *entrada, ExpressaoLIA *out, int n_vars);
+bool avaliar_lia (const ExpressaoLIA *exp, int *valores);
+int checagem_lia(int *atrib_bool, TeoriaLIA *teoria);
+
+/*funcoes da arvore de busca e a execucao principal*/
+tree *no_da_arvore();
+tree *resposta_smt(CNF *formula, partial_interpretation now, TeoriaLIA *teoria);
+
+/*as funcoes de limpeza e de saida*/
+bool imprimir(tree *node, int total_literals);
+void free_tree(tree *node);
+void free_cnf(CNF *formula);
+
 #endif

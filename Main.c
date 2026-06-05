0c29c411db37baf11bec94ab090f94ba7af61d04
CarolinaCavalcantee <accj@ic.ufal.br>
2026-06-04 21:30:28 -0300

    alteracao inicial - teoriaLIA

--- Main.c
+++ Main.c
@@ -2,27 +2,43 @@
 #include "SMT_solver.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(){
 
-    CNF *formula = (CNF*)malloc(sizeof(CNF)); //aloca memoria para formula
-    teoria_smt *teoria = (teoria_smt*)malloc(sizeof(teoria_smt));
+    CNF *formula = (CNF*)malloc(sizeof(CNF)); //aloca memoria e inicializa a formula
+    formula->clauses = NULL;
+    formula->total_literals = 0;
+    formula->total_clauses = 0;
 
-    teoria-> total_expressoes = 0;
-    teoria->expressoes = NULL; /*inicializacao segura da teoria, garante q tudo ta zerado*/
-    
+    TeoriaLIA *teoria = malloc(sizeof(TeoriaLIA));/*aloca e inicializa a teoriaLIA*/
+    teoria->total = 0;
+    teoria->n_vars = 0;
+    teoria->restricoes = NULL;
+    teoria->valores_int = NULL;
+
     readcnffile("arquivo.cnf", formula, teoria);//chama funcao para leitura do arquivo    
     
-    if (formula->clauses == NULL && teoria->expressoes == NULL){
+    if (formula->clauses == NULL && teoria->restricoes == NULL){
         printf("Erro: Arquivo vazio ou nao encontrado.\n");
+        free(formula);
+        free(teoria);
         return 1;
 }
 
-printf("--- TESTE DE LEITURA ---\n");
-printf("Total de literais lidos: %d\n", formula->total_literals);
-printf("Total de clausulas lidas: %d\n", formula->total_clauses);
-printf("Total de expressoes na teoria: %d\n", teoria->total_expressoes);
-printf("------------------------\n");
+printf("--- LEITURA ---\n");
+printf("Variaveis booleanas: %d\n", formula->total_literals);
+printf("Clausulas: %d\n", formula->total_clauses);
+printf("Restricoes LIA: %d\n", teoria->total);
+printf("Variaveis inteiras: %d\n", teoria->n_vars);
+
+if(teoria->n_vars > 0 && teoria->valores_int){
+    printf("Valores inteiros: ");
+    for (int i = 0; i < teoria->n_vars; i++){
+        printf (" x%d = %d", i + 1, teoria->valores_int[i]);
+    }
+    printf("\n");
+}
+   printf("---------------\n");
+
     partial_interpretation options = inicio_partial_interpretation(formula); //inicializaçao da interpretacao parcial
     
     /*a gnt comeca a teoria vazia p nn dar erro*/
@@ -39,8 +55,9 @@ printf("------------------------\n");
 
    free_tree(resposta);
    free(options.atributions);
+   free(teoria->restricoes);
+   free(teoria->valores_int);
    free_cnf(formula);
-
    free(teoria);
    return 0;
 }
